ad8de3719007ef0594ed782d9e7b079b0b2c2f9e
Asteven-zn <964038167@qq.com>
2020-06-03 12:37:20 +0800

    commit

--- src/03vars/demon02/02varstest.go
+++ src/03vars/demon02/02varstest.go
@@ -1,15 +1,17 @@
 package main
+
 import (
 	"fmt"
 	"unsafe"
 )
+
 func main() {
 	//%T 输出值的类型，表示为十进制
 	//%d 输出数据所占用的字节数
 	var n1 = -1000000
-	fmt.Printf("%T \n%d",n1,unsafe.Sizeof(n1))
+	fmt.Printf("%T \n%d", n1, unsafe.Sizeof(n1))
 	n2 := 20
-	fmt.Println("\nn2 =",n2)
+	fmt.Println("\nn2 =", n2)
 
 	// var c1 byte = 'a'
 	// var c2 byte = '3'
